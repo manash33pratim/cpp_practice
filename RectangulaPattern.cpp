3046421b4ded3013dcfd183929658affe050ea2c
manash33pratim <manash33pratim@gmail.com>
2023-01-27 21:08:49 +0530

    adding //

--- RectangulaPattern.cpp
+++ RectangulaPattern.cpp
@@ -18,7 +18,7 @@ int main(){
 
             }
             else
-            cout<<" ";//FOR PRINTING " "
+            cout<<" ";//FOR PRINTING " "/////////////
         }
         cout<<endl;
     }
